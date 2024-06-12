d5f287bca74bcef41ded58a05c5f6a524b54deb6
Lawrence Amoafo <rekenburg777@gmail.com>
2024-06-12 16:48:19 +0000

    Implement the menu section populated with dummy data

---
+++ app/ui/dashboard/meals/cards.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+
+const foodArray = [
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 1',
+    description: 'Description of Food 1',
+    totalSold: 10,
+    price: 9.99,
+  },
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 2',
+    description: 'Description of Food 2',
+    totalSold: 5,
+    price: 12.99,
+  },
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 3',
+    description: 'Description of Food 3',
+    totalSold: 5,
+    price: 12.99,
+  },
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 4',
+    description: 'Description of Food 4',
+    totalSold: 5,
+    price: 12.99,
+  },
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 5',
+    description: 'Description of Food 5',
+    totalSold: 5,
+    price: 12.99,
+  },
+  {
+    image: '/Spanish-Tortilla.jpg',
+    name: 'Food 6',
+    description: 'Description of Food 6',
+    totalSold: 5,
+    price: 12.99,
+  },
+];
+
+export default function FoodCards() {
+  return (
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      {foodArray.map((food, index) => (
+        <div key={index} className='p-4 bg-white shadow-md rounded-md'>
+          <div className='flex justify-between'>
+            <div>
+            <Image src={food.image} alt={food.name} width={150} height={100} className='object-cover rounded-md' />
+            </div>
+            <div className='flex flex-col justify-between'>
+              <div>
+                <h2 className='text-xl font-semibold'>{food.name}</h2>
+                <p className='text-sm text-slate-600'>{food.description}</p>
+              </div>
+              <div className='flex justify-between'>
+                <p className='text-lg font-semibold'>${food.price}</p>
+                <p className='text-lg font-semibold'>{food.totalSold} sold</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
