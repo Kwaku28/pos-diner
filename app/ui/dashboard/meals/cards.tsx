829019bd017f8d821ccec93c21db9240bb343971
Lawrence Amoafo <rekenburg777@gmail.com>
2024-06-13 18:00:11 +0000

    Edit the css properties of the card elements

--- app/ui/dashboard/meals/cards.tsx
+++ app/ui/dashboard/meals/cards.tsx
@@ -47,21 +47,21 @@ const foodArray = [
 
 export default function FoodCards() {
   return (
-    <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+    <div className='grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3'>
       {foodArray.map((food, index) => (
         <div key={index} className='p-4 bg-white shadow-md rounded-md'>
-          <div className='flex justify-between'>
+          <div className='flex flex-col justify-between'>
             <div>
             <Image src={food.image} alt={food.name} width={150} height={100} className='object-cover rounded-md' />
             </div>
             <div className='flex flex-col justify-between'>
               <div>
-                <h2 className='text-xl font-semibold'>{food.name}</h2>
+                <h2 className='text-xl font-semibold text-center'>{food.name}</h2>
                 <p className='text-sm text-slate-600'>{food.description}</p>
               </div>
-              <div className='flex justify-between'>
+              <div className='flex justify-between items-center'>
                 <p className='text-lg font-semibold'>${food.price}</p>
-                <p className='text-lg font-semibold'>{food.totalSold} sold</p>
+                <p className='text-sm font-semibold text-slate-400'>{food.totalSold} sold</p>
               </div>
             </div>
           </div>
