c8be7ce886019723a6fc14dea10c110892e8ab47
Lawrence Amoafo <rekenburg777@gmail.com>
2024-06-17 13:25:03 +0000

    Update meals.tsx

--- app/ui/dashboard/meals/meals.tsx
+++ app/ui/dashboard/meals/meals.tsx
@@ -3,9 +3,9 @@ import FoodCards from "./cards";
 
 export default function Meals() {
   return (
-    <div className="mt-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Menu</h2>
-      <div>
+    <div className="mt-2">
+      <h2 className={`${lusitana.className} mb-2 text-xl font-bold md:text-2xl`}>Select Menu</h2>
+      <div className="flex max-h-80 overflow-y-auto">
         <FoodCards />
       </div>
     </div>
